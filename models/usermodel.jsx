7d3dd17c8cfb66c2b0d599ca7a53a2a91a95abab
Raju Shrestha <facebook101022222@gmail.com>
2025-01-11 21:27:47 +0545

    cart

--- models/usermodel.jsx
+++ models/usermodel.jsx
@@ -7,10 +7,10 @@ const userSchema = mongoose.Schema({
     },
     email : String,
     password :String,
-    cart : {
-        type : Array,
-        default : []
-    },
+    cart : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "product",
+    }],
     
     orders : {
         type : Array,
--- routes/index.jsx
+++ routes/index.jsx
@@ -6,7 +6,7 @@ const productModel = require("../models/productmodel.jsx")
 
 router.get("/" , (req,res) =>{
     const error = req.flash("error");
-    res.render("index", {error});
+    res.render("index", {error,isLoggedin: false});
     
 })
 
@@ -14,6 +14,9 @@ router.get("/shop", isLoggedin, async (req,res) =>{
 
     let product = await productModel.find()
     res.render("shop",{ product });
+})
+router.get("/addtocart/:id", isLoggedin, async(req,res)=>{
+  let user= await userModel.findOne({user: req.user.email})
 })
 
 router.get("/logout", isLoggedin, (req,res) =>{
